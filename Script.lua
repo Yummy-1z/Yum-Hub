bdb6e111920c9ffa7b1a054a95738e97f32220fe
Yummy-1z <kalamalum744@gmail.com>
2025-03-18 17:31:34 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2,9 +2,9 @@ local Url = "https://raw.githubusercontent.com/Yummy-1z/Yum-Hub/refs/heads/main/
 local Charts = {
     -- [ Speed Piece ] --
     [74193535211122] = "Speed-Piece.lua",
-    [77167260540689] = "Speed-Piece.lua" -- [ Dungeon ]
+    [77167260540689] = "Speed-Piece.lua", -- [ Dungeon ]
     -- [ Lucky Blocks Battlegrounds ] --
-    [662417684] = "LUCKY-BLOCKS.lua"
+    [662417684] = "LUCKY-BLOCKS.lua",
 }
 for i, v in pairs(Charts) do
     if i == game.PlaceId or i == game.GameId then
