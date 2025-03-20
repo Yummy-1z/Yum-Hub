13c7f8538195ff29c5e1b1ce435054f8686a7858
Yummy-1z <kalamalum744@gmail.com>
2025-03-20 15:27:18 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,4 @@
+repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.PlayerGui
 local Url = "https://raw.githubusercontent.com/Yummy-1z/Yum-Hub/refs/heads/main/Charts/"
 local Charts = {
     -- [ Speed Piece ] --
@@ -6,8 +7,16 @@ local Charts = {
     -- [ Lucky Blocks Battlegrounds ] --
     [662417684] = "LUCKY-BLOCKS.lua",
 }
-for i, v in pairs(Charts) do
-    if i == game.PlaceId or i == game.GameId then
-        loadstring(game:HttpGet(Url .. v))()
+
+for PlaceId, Chart in pairs(Charts) do
+    if PlaceId == game.PlaceId or PlaceId == game.GameId then
+        loadstring(game:HttpGet(Url .. Chart))()
     end
 end
+
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+    VirtualUser:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	wait(1)
+	VirtualUser:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
